466524bbc2532440763d9220d16d332b028fd62f
Derek Surguy <Surguy.Derek@gmail.com>
2021-12-24 07:07:53 -0600

    Read data for day 7

---
+++ day-7/index.ts
@@ -0,0 +1,42 @@
+import { readFile } from 'fs';
+import path from 'path';
+import { Arguments } from 'yargs-parser';
+import { isInspecting } from '../utils';
+
+type Data = number[]
+
+async function readData(useSimpleInput: boolean): Promise<Data> {
+  return new Promise((resolve, reject) => {
+    readFile(path.resolve(__dirname, useSimpleInput ? 'simpleInput': 'input'), (err, data) => {
+      if( err ) reject(err);
+      else {
+        resolve(
+          data
+            .toString()
+            .split(/,/g)
+            .filter(v => v)
+            .map(v => parseInt(v, 10))
+        );        
+      }
+    })
+  })
+}
+
+function getPowerToAlign(data: number[]) {
+  let positions = data.slice(0);
+}
+
+async function runtime(argv: Arguments) {
+  if( isInspecting() ) debugger;
+  try {
+    const data = await readData(!!argv.simple);
+    console.log("Data Read", data.length)
+  } catch (e) {
+    console.error("Unable to read input data")
+    throw e;
+  }
+}
+
+export default {
+  run: (argv: Arguments) => runtime(argv)
+}
