48f2affa6a198ef8d06c187320f87bff49556ff1
Derek Surguy <Surguy.Derek@gmail.com>
2021-12-26 10:27:07 -0600

    Solutions to day 7

--- day-7/index.ts
+++ day-7/index.ts
@@ -22,15 +22,42 @@ async function readData(useSimpleInput: boolean): Promise<Data> {
   })
 }
 
-function getPowerToAlign(data: number[]) {
-  let positions = data.slice(0);
+function getMedianPowerToAlign(data: number[]) {
+  let positions = data.slice(0).sort((a,b) => a < b ? -1 : a > b ? 1 : 0);
+  if( positions.length % 2 === 0 ){
+    let floorMedian = positions[positions.length/2 - 1];
+    let ceilMedian = positions[positions.length/2];
+    let floorCost = positions.reduce((sum, p) => sum + Math.abs(p - floorMedian), 0);
+    let ceilCost = positions.reduce((sum, p) => sum + Math.abs(p - ceilMedian), 0);
+    return Math.min(floorCost, ceilCost);
+  }
+  else {
+    let median = positions[Math.floor(positions.length/2)];
+    return positions.reduce((sum, p) => sum + Math.abs(p - median));
+  }
+}
+
+function getMeanPowerToAlign(data: number[]) {
+  let mean = data.reduce((sum, p) => sum + p, 0)/data.length;
+  let floor = Math.floor(mean);
+  let ceil = Math.ceil(mean);
+  let floorCost = data.reduce((sum, p) => {
+    let diff = Math.abs(p - floor);
+    return sum + diff * (1 + diff) / 2;
+  }, 0);
+  let ceilCost = data.reduce((sum, p) => {
+    let diff = Math.abs(p - ceil);
+    return sum + diff * (1 + diff) / 2;
+  }, 0);
+  return Math.min(floorCost, ceilCost);
 }
 
 async function runtime(argv: Arguments) {
   if( isInspecting() ) debugger;
   try {
     const data = await readData(!!argv.simple);
-    console.log("Data Read", data.length)
+    console.log("Median power to align", getMedianPowerToAlign(data));
+    console.log("Mean power to align", getMeanPowerToAlign(data));
   } catch (e) {
     console.error("Unable to read input data")
     throw e;
